4cbb1a5b3a58acff4444658d790dccc9ef15491b
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-01-21 19:56:19 -0200

    pymongo

---
+++ python-pymongo/Server.py
@@ -0,0 +1,85 @@
+# python -B Server.py --help
+# python -B Server.py --logging=debug
+
+# http localhost:8888/
+# http localhost:8888/dynamic
+# http localhost:8888/static/index.html
+# http localhost:8888/query
+
+from logging import DEBUG, getLogger as log
+from multiprocessing.pool import ThreadPool
+from os.path import dirname, join
+from signal import signal, SIGTERM
+from sys import exit
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line, parse_config_file
+from tornado.web import Application
+
+from traceback import format_exc
+from uuid import uuid1
+
+from handlers import *
+
+define("config", help="config file", default=None, type=str)
+define("address", help="binding host", default="localhost", type=str)
+define("port", help="binding port", default=8888, type=int)
+
+define("db_pool", help="database pool-id", default="stock", type=str)
+define("db_name", help="database name", default="stock", type=str)
+define("db_host", help="database host", default="localhost", type=str)
+define("db_port", help="database port", default=27017, type=int)
+
+def shutdown():
+    io_loop = IOLoop.instance()
+    if io_loop.running():
+        io_loop.stop()
+    log().info("server stopped")
+
+def on_signal(sig, frame):
+    if http_server != None:
+        log().info("shutting down server...")
+        http_server.stop()
+    IOLoop.instance().add_callback(shutdown)
+
+def main():
+    global http_server
+
+    try:
+        signal(SIGTERM, on_signal)
+        
+        parse_command_line()
+        if options.config != None:
+            parse_config_file(options.config)
+
+        path = join(dirname(__file__), "templates")
+
+        application = Application([
+            (r'/', IndexHandler),
+            (r'/stock', StockHandler)],
+            template_path=path, 
+            static_path=join(dirname(__file__), "static"))
+
+        application.options = options
+        application.workers = ThreadPool(100)
+
+        http_server = HTTPServer(application)
+        http_server.listen(options.port, options.address)
+        log().info("server listening on port %s:%d" % 
+            (options.address, options.port))
+        if log().isEnabledFor(DEBUG):
+            log().debug("autoreload enabled")
+            tornado.autoreload.start()        
+        IOLoop.instance().start()
+
+    except KeyboardInterrupt:
+        log().info("exiting...")
+
+    except BaseException as ex:
+        log().error("exiting due: [%s][%s]" % 
+            (str(ex), str(format_exc().splitlines())))
+        exit(1)
+
+if __name__ == '__main__':
+    main()
---
+++ python-pymongo/handlers/Base.py
@@ -0,0 +1,66 @@
+from bson import json_util
+from datetime import datetime
+from json import dumps
+from logging import getLogger as log
+from pymongo import MongoClient
+
+from tornado.ioloop import IOLoop
+from tornado.template import Loader
+from tornado.web import asynchronous, RequestHandler, HTTPError
+
+from types import ListType
+
+class IndexHandler(RequestHandler):
+    @asynchronous
+    def get(self):
+        self.render('index.html')
+
+class StockHandler(RequestHandler):
+    def run_background(self, func, callback, args=(), kwds={}):
+        def _callback(result):
+            IOLoop.instance().add_callback(lambda: callback(result))
+        self.application.workers.apply_async(func, args, kwds, _callback)
+
+    def distinct(self, args):
+        options = self.application.options
+        connection = MongoClient(options.db_host, options.db_port, 
+            max_pool_size = 0, auto_start_request = True, use_greenlets = True)
+        db = connection[options.db_name]
+        response = db.command({"distinct": "symbols", "key": "S"})
+        connection.end_request()
+        return response
+
+    def find(self, symbol):
+        options = self.application.options
+        connection = MongoClient(options.db_host, options.db_port, 
+            max_pool_size = 0, auto_start_request = True, use_greenlets = True)
+        db = connection[options.db_name]
+        response = db.symbols.find({'S': symbol})
+        connection.end_request()
+        return response
+
+    @asynchronous
+    def get(self):
+        symbol = self.get_argument("symbol", None)
+        if symbol == None:
+            self.run_background(self.distinct, self._on_symbols, (None,))
+        else:
+            self.run_background(self.find, self._on_data, (symbol,))
+
+    def _on_symbols(self, response):
+        self.set_header("Content-Type", "application/json; charset=UTF-8")
+        self.write(''.join([self.get_argument("callback"), 
+                   "([", dumps(response["values"], default=json_util.default), "]);"]))
+        self.finish()
+
+    def _on_data(self, response):
+        self.set_header("Content-Type", "application/json; charset=UTF-8")
+        lst = [[self.get_argument("symbol")]]
+        for t in response:
+            date = map(int, t["D"].split("-"))
+            data = [int(datetime(date[0],date[1],date[2]).strftime('%s')) * 1000]
+            data.extend([float(t[x]) for x in ["O", "H", "L", "C", "V"]])
+            lst.append(data)  
+        self.write(''.join([self.get_argument("callback"),
+                   "(", dumps(lst, default=json_util.default), ");"]))
+        self.finish()
